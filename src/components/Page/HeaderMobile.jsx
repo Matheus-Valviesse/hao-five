784e7cbb0afe69f5f352c521e3570fab9c780687
matheus-valviesse <mateusvalviesse@gmail.com>
2024-07-16 19:01:45 -0300

    mobile modal

--- src/components/Page/HeaderMobile.jsx
+++ src/components/Page/HeaderMobile.jsx
@@ -1,6 +1,8 @@
 "use client";
 import Logo from "@/assets/Logo 2.svg";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+
 const LIs = ({ text }) => {
   return (
     <motion.li
@@ -20,7 +22,22 @@ const LIs = ({ text }) => {
     </motion.li>
   );
 };
+
+const MobileOption = () => {
+  return (
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 50 }}
+        className="w-full h-full fixed bg-black bottom-0"
+      ></motion.div>
+    </AnimatePresence>
+  );
+};
+
 const HeaderMobile = () => {
+  const [mobileMoldal, setMobileModal] = useState(false);
   const headerLinks = [
     { name: "Home", path: "" },
     { name: "Sobre nós", path: "" },
@@ -28,13 +45,18 @@ const HeaderMobile = () => {
     { name: "Serviços", path: "" },
     { name: "Contato", path: "" },
   ];
+
   return (
     <header className=" w-full h-[100px] px-[10px] bg-[#fff] flex sm:hidden flex-row justify-between items-center">
+      {mobileMoldal && <MobileOption />}
       <span className=" w-[180px]">
         <Logo className="  w-full -mt-2" />
       </span>
 
-      <button className="flex flex-col gap-1 ">
+      <button
+        className="flex flex-col gap-1 "
+        onClick={() => setMobileModal(true)}
+      >
         <span className="w-8 h-1 rounded-md bg-black"></span>
         <span className="w-8 h-1 rounded-md bg-black"></span>
         <span className="w-8 h-1 rounded-md bg-black"></span>
