77ceb98186c3b658666d1b144857b429311d4538
valviesse <mateusvalviesse@gmail.com>
2024-07-26 00:10:52 -0300

    image test

--- src/components/button/SecondaryButton.jsx
+++ src/components/button/SecondaryButton.jsx
@@ -6,14 +6,14 @@ const SecondaryButton = () => {
     <motion.div className='relative w-[150px] h-[40px] bg-[#000f] text-[#fff] text-[14px] font-bold overflow-hidden flex items-center justify-center cursor-pointer'      
       initial="hidden"
       whileHover="visible"
-      transition={{ duration: 0.3 }}>
+      transition={{ duration: 0.6 }}>
       <p className='absolute z-20'>DESCUBRA MAIS </p>
       <motion.span
         className="absolute bottom-0 left-0 w-full h-full bg-[#FF14FF] origin-left z-10"
         variants={{
           hidden: { scaleX: 0.04},
           visible: { scaleX: 1 },
-        }}
+        }}transition={{ duration: 0.3 }}
       /></motion.div>
   )
 }
--- src/components/home/thirdCard/ThirdCard.jsx
+++ src/components/home/thirdCard/ThirdCard.jsx
@@ -1,12 +1,18 @@
+'use client'
 import React from 'react'
 import ArrowLeft from '@/assets/button/arrow-left.svg'
 import ArrowRight from '@/assets/button/arrow-right.svg'
-import ProjectImg from '@/assets/images/bg/project.svg'
+import c from '@/assets/captura.png'
+import Image from 'next/image'
 const ProjectCard = () =>{
 return (
-  <div className='w-full flex flex-row min-h-[300px] h-full  overflow-hidden bg-blue-700'>
-    <div className='bg-white flex flex-col text-[#1C1C1C]  py-[30px] px-[60px]  max-w-[60vw] justify-start items-start gap-y-4'>
+  <div className='w-full flex min-h-[280px] h-full overflow-hidden '>
+    <div className='bg-white flex flex-col text-[#1C1C1C]  py-[30px] px-[60px]  sm:max-w-[60vw] justify-start items-start gap-y-4'>
+      <span className='max-h-[200px] h-full w-full block sm:hidden '>
+        {/* <ProjectImg  className='w-fit max-h-[200px] bg-cover '/> */}
+      </span>
       <h3 className='text-[28px]  font-semibold uppercase'>propjeto 01</h3>
+
       <p className='text-[16px] text-[#2F2F2F] leading-[18px] font-medium'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <button className='uppercase justify-start flex gap-2 text-[18px] font-bold'>
         conheça 
@@ -15,39 +21,44 @@ return (
      
       </button>
     </div>
-  <span className='max-w-[40vw] max-h-full bg-red-50 bg-cover'><ProjectImg  className='max-w-full w-screen  h-full bg-cover  object-cover'/></span>
+  
+    {/* <ProjectImg className='max-h-[280px] max-w-[40vw] h-full w-full bg-fuchsia-900'/> */}
    
+     <Image src={'/captura.png'} width={100} height={100} alt='a'/>
+   
+     <Image src={c} width={100} height={100} alt='a'/>
+    
   </div>
 )
 }
 
-const ProjectCardReverse = () =>{
-return (
-  <div className='w-full flex flex-row max-h-[300px] overflow-hidden'> 
-    <ProjectImg  className='max-w-[40vw]'/>
-    <div className='bg-[#221D1D] flex flex-col text-[#EEEEEE]  py-[30px] px-[60px]  max-w-[60vw] justify-start items-end text-end gap-y-4'>
-      <h3 className='text-[28px]  font-semibold uppercase'>propjeto 01</h3>
-      <p className='text-[16px] text-[#dddddd] leading-[18px] font-medium'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <button className='uppercase justify-start flex gap-2 text-[18px] font-bold'>
+// const ProjectCardReverse = () =>{
+// return (
+//   <div className='w-full flex flex-row max-h-[300px] overflow-hidden'> 
+//     <ProjectImg  className='max-w-[40vw]'/>
+//     <div className='bg-[#221D1D] flex flex-col text-[#EEEEEE]  py-[30px] px-[60px]  max-w-[60vw] justify-start items-end text-end gap-y-4'>
+//       <h3 className='text-[28px]  font-semibold uppercase'>propjeto 01</h3>
+//       <p className='text-[16px] text-[#dddddd] leading-[18px] font-medium'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+//       <button className='uppercase justify-start flex gap-2 text-[18px] font-bold'>
         
        
-          <ArrowRight className='w-6'/>conheça 
+//           <ArrowRight className='w-6'/>conheça 
      
-      </button>
-    </div>
+//       </button>
+//     </div>
 
   
-  </div>
-)
-}
+//   </div>
+// )
+// }
 const ThirdCard = () => {
   return (
     <div className='bg-black w-full flex flex-col'>
     <h2 className='uppercase text-[34px] text-[#D9D9D9] font-semibold my-[60px] text-center'>Confira nossos projetos!</h2>
     <div className='flex flex-col gap-y-20'>
       <ProjectCard />
-      <ProjectCardReverse />
-      <ProjectCard />
+      {/* <ProjectCardReverse />
+      <ProjectCard /> */}
     </div>
   
     </div>
