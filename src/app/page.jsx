0b636ffeb00607cd630321154e648eb951e530c4
valviesse <mateusvalviesse@gmail.com>
2024-08-29 18:31:33 -0300

    error cards

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import FirstCard from '@/components/home/firstCard/FirstCard';
 import SecondCard from '@/components/home/secondCard/SecondCard';
 import ThirdCard from '@/components/home/thirdCard/ThirdCard';
 import FourthCard from '@/components/home/fourthCard/FourthCard';
-import FifthCard from '@/components/home/fifthCard/fifthCard';
+import FifthCard from '@/components/home/fifthCard/FifthCard';
 
 export default function Home() {
 	return (
